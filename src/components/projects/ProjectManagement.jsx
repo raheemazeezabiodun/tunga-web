17d903959338612e0d6f1ff21082d1918574f261
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2018-07-11 19:58:38 +0100

    Create Team component and right details column for projects workflow page  (#362)

--- src/components/projects/ProjectManagement.jsx
+++ src/components/projects/ProjectManagement.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Switch, Route, Redirect, NavLink } from 'react-router-dom';
+import moment from 'moment';
 
 import Activity from './Activity';
 import Docs from './Docs';
@@ -8,6 +9,7 @@ import Team from './Team';
 import Plan from './Plan';
 import Pay from './Pay';
 import Settings from './Settings';
+import Avatar from '../core/Avatar';
 
 export default class ProjectManagement extends React.Component {
     static propTypes = {
@@ -61,10 +63,22 @@ export default class ProjectManagement extends React.Component {
                         </div>
                     </div>
                     <div className="project-details float-right">
-                        <h6>{project.title}</h6>
+                        <h6 className="project-title">{project.title}</h6>
 
                         <h6>Description</h6>
-                        <div>{project.description}</div>
+                        <p>{project.description || 'No description'}</p>
+
+                        <h6>Deadline</h6>
+                        <p>{project.deadline ? moment(project.deadline).format('Do of MMMM YYYY'): 'Deadline not set'}</p>
+
+                        <h6>Team</h6>
+                        <p>Project Owner</p>
+                        <div>{project.owner ? <Avatar image={project.owner.avatar_url} /> : null}</div>
+                        <p>Team</p>
+                        <div>{project.participation.map(team => {
+                                return <Avatar key={`Team ${team.user.id}`} image={team.user.avatar_url} />
+                            })}
+                        </div>
                     </div>
                 </div>
             ):null
---
+++ src/components/projects/modals/ProjectMemberForm.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {FormGroup, Row} from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import UserSelector from '../../core/UserSelector';
+import Button from '../../core/Button';
+
+
+export default class ProjectMemberForm extends React.Component {
+    static propTypes = {
+        project: PropTypes.object,
+        ProjectActions: PropTypes.object,
+        user: PropTypes.string,
+        dismiss: PropTypes.func
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: []
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.isSaved[this.props.project.id]) {
+            this.props.dismiss(); // close the modal
+        }
+    }
+
+    onSelectUser = (selected) => {
+        if (this.props.user === 'participation') {
+            this.setState({ user: selected });
+        } else {
+            this.setState({ user: selected[0].id })
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const {ProjectActions, project, user} = this.props;
+        const user_type = {};
+        if (user === 'participation') {
+            let users = [];
+            this.state.user.map((data) => {
+                let user = {id: data.id}
+                users.push({user});
+            });
+            user_type[user] = users;
+        } else {
+            user_type[user] = {id: this.state.user}
+        }
+        ProjectActions.updateProject(project.id, {
+            ...user_type
+        });
+    }
+
+    render() {
+        return (
+            <div className="project-team">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <UserSelector account_type={this.props.type}
+                            onChange={this.onSelectUser.bind(this)}
+                            variant='bottom'
+                            max={this.props.max}
+                        />
+                    </FormGroup>
+                    <div className="float-right add-button">
+                        <Button onClick={this.handleSubmit} disabled={this.props.isSaving[this.props.project.id]}>save</Button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}

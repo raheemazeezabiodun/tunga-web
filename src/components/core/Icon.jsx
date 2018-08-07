d32f9ada4a467352c1aef1ddf1cd142ced3876c2
david <tdsemakula@gmail.com>
2018-08-07 03:57:46 +0300

    make icons clickable

--- src/components/core/Icon.jsx
+++ src/components/core/Icon.jsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import {addEventListeners, BUTTON_EVENTS} from './utils/events';
+import {filterButtonProps} from "./utils/forms";
+
 export default class Icon extends React.Component {
     static propTypes = {
         name: PropTypes.string,
@@ -10,7 +13,9 @@ export default class Icon extends React.Component {
 
     render() {
         return (
-            <i className={`tg-ic-${this.props.name || ''} ${this.props.size?`tunga-ic-sz-${this.props.size}`:''} ${this.props.className || ''}`}/>
+            <i className={`tg-ic-${this.props.name || ''} ${this.props.size?`tunga-ic-sz-${this.props.size}`:''} ${this.props.className || ''}`}
+               {...filterButtonProps(this.props)}
+               {...addEventListeners(BUTTON_EVENTS, this.props)}/>
         );
     }
 }

da51d53c4bb56b7644bb1b1560b0c80e7349eba9
david <tdsemakula@gmail.com>
2018-09-06 16:05:15 +0300

    create client UI

--- src/components/dashboard/network/UserForm.jsx
+++ src/components/dashboard/network/UserForm.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import {FormGroup} from 'reactstrap';
+import {FormGroup, Row, Col} from 'reactstrap';
 import CustomInputGroup from "../../core/CustomInputGroup";
 import FieldError from "../../core/FieldError";
 import Select from "../../core/Select";
 import Button from "../../core/Button";
 import Progress from "../../core/Progress";
 import Success from "../../core/Success";
+import CountrySelector from '../../core/CountrySelector';
+import ChoiceGroup from "../../core/ChoiceGroup";
+import InputGroup from "../../core/InputGroup";
+import Icon from "../../core/Icon";
+import Input from "../../core/Input";
 
 import {USER_TYPE_CHOICES} from "../../../actions/utils/api";
 import connect from "../../../connectors/AuthConnector";
+
+const TYPE_INVITE = 'invite';
+const TYPE_CLIENT = 'client';
 
 class UserForm extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {user: {}};
+        this.state = {user: {}, type: TYPE_INVITE};
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -37,7 +45,11 @@ class UserForm extends React.Component {
         e.preventDefault();
 
         const {AuthActions} = this.props;
-        AuthActions.invite(this.state.user);
+        if(this.state.type === TYPE_INVITE) {
+            AuthActions.invite(this.state.user);
+        } else {
+            // TODO: Create client
+        }
     };
 
     render() {
@@ -53,6 +65,16 @@ class UserForm extends React.Component {
                     {isInviting?(
                         <Progress/>
                     ):null}
+
+                    <FormGroup>
+                        <ChoiceGroup variant="outline-primary"
+                                     selected={this.state.type}
+                                     choices={[
+                                         [TYPE_INVITE, 'Invite User'],
+                                         [TYPE_CLIENT, 'Create Client']
+                                     ]}
+                                     onChange={(type) => { this.setState({type}); }}/>
+                    </FormGroup>
 
                     <FormGroup>
                         <label>Email: *</label>
@@ -72,47 +94,144 @@ class UserForm extends React.Component {
                         <CustomInputGroup variant="personal"
                                           onChange={this.onChangeField.bind(this, 'first_name')}
                                           value={this.state.user.first_name}
+                                          placeholder="First Name"
                                           required/>
                     </FormGroup>
                     <FormGroup>
-                        <label>First Name: *</label>
+                        <label>Last Name: *</label>
                         {errors && errors.invite && errors.invite.last_name ? (
                             <FieldError message={errors.invite.last_name} />
                         ) : null}
                         <CustomInputGroup variant="personal"
                                           onChange={this.onChangeField.bind(this, 'last_name')}
                                           value={this.state.user.last_name}
+                                          placeholder="Last Name"
                                           required/>
                     </FormGroup>
-                    <FormGroup>
-                        <label>User Type: *</label>
-                        {errors && errors.invite && errors.invite.type ? (
-                            <FieldError message={errors.invite.type} />
-                        ) : null}
-                        <Select options={USER_TYPE_CHOICES.map(item => {return [item.id, item.name]})}
-                                onChange={value => this.onChangeValue('type', value)}
-                                selected={this.state.user.type}
-                                required/>
-                    </FormGroup>
-                    <FormGroup>
-                        {errors && errors.invite && errors.invite.resend ? (
-                            <FieldError message={errors.invite.resend} />
-                        ) : null}
-                        <div className="form-check">
-                            <input className="form-check-input"
-                                   id="resend-user-invite"
-                                   type="checkbox"
-                                   checked={this.state.resend}
-                                   onChange={e => this.onChangeValue('resend', e.target.checked)}
-                            />
-                            <label className="form-check-label" htmlFor="resend-user-invite">
-                                Resend Invite if user is already invited
-                            </label>
-                        </div>
-                    </FormGroup>
-                    <FormGroup>
-                        <Button type="submit">Invite User</Button>
-                    </FormGroup>
+                    {this.state.type === TYPE_INVITE?(
+                        <React.Fragment>
+                            <FormGroup>
+                                <label>User Type: *</label>
+                                {errors && errors.invite && errors.invite.type ? (
+                                    <FieldError message={errors.invite.type} />
+                                ) : null}
+                                <Select options={USER_TYPE_CHOICES.map(item => {return [item.id, item.name]})}
+                                        onChange={value => this.onChangeValue('type', value)}
+                                        selected={this.state.user.type}
+                                        required/>
+                            </FormGroup>
+                            <FormGroup>
+                                {errors && errors.invite && errors.invite.resend ? (
+                                    <FieldError message={errors.invite.resend} />
+                                ) : null}
+                                <div className="form-check">
+                                    <input className="form-check-input"
+                                           id="resend-user-invite"
+                                           type="checkbox"
+                                           checked={this.state.resend}
+                                           onChange={e => this.onChangeValue('resend', e.target.checked)}
+                                    />
+                                    <label className="form-check-label" htmlFor="resend-user-invite">
+                                        Resend Invite if user is already invited
+                                    </label>
+                                </div>
+                            </FormGroup>
+                            <FormGroup>
+                                <Button type="submit">Invite User</Button>
+                            </FormGroup>
+                        </React.Fragment>
+                    ):(
+                        <React.Fragment>
+                            <FormGroup>
+                                <label>Company: *</label>
+                                {errors && errors.invite && errors.invite.company ? (
+                                    <FieldError message={errors.invite.company} />
+                                ) : null}
+                                <Input onChange={this.onChangeField.bind(this, 'company')}
+                                       value={this.state.user.company}
+                                       placeholder="Company"
+                                       required/>
+                            </FormGroup>
+                            <Row>
+                                <Col className="col-main">
+                                    <FormGroup>
+                                        <label>Street: *</label>
+                                        {errors && errors.invite && errors.invite.street ? (
+                                            <FieldError message={errors.invite.street} />
+                                        ) : null}
+                                        <InputGroup prepend={(<Icon name="map-marker"/>)}
+                                                    onChange={this.onChangeField.bind(this, 'street')}
+                                                    value={this.state.user.street}
+                                                    placeholder="Street"
+                                                    required/>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={3}>
+                                    <FormGroup>
+                                        <label>Number/Plot: *</label>
+                                        {errors && errors.invite && errors.invite.plot_number ? (
+                                            <FieldError message={errors.invite.plot_number} />
+                                        ) : null}
+                                        <Input onChange={this.onChangeField.bind(this, 'plot_number')}
+                                               value={this.state.user.plot_number}
+                                               placeholder="Number/Plot"
+                                               required/>
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col className="col-main">
+                                    <FormGroup>
+                                        <label>City: *</label>
+                                        {errors && errors.invite && errors.invite.city ? (
+                                            <FieldError message={errors.invite.city} />
+                                        ) : null}
+                                        <InputGroup prepend={(<Icon name="map-marker"/>)}
+                                                    onChange={this.onChangeField.bind(this, 'city')}
+                                                    value={this.state.user.city}
+                                                    placeholder="City"
+                                                    required/>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={3}>
+                                    <FormGroup>
+                                        <label>Zip code: *</label>
+                                        {errors && errors.invite && errors.invite.postal_code ? (
+                                            <FieldError message={errors.invite.postal_code} />
+                                        ) : null}
+                                        <Input onChange={this.onChangeField.bind(this, 'postal_code')}
+                                               value={this.state.user.postal_code}
+                                               placeholder="Zip code"
+                                               required/>
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+                            <FormGroup>
+                                <label className="control-label">Country: *</label>
+                                {errors && errors.invite && errors.invite.country ? (
+                                    <FieldError message={errors.invite.country} />
+                                ) : null}
+                                <CountrySelector
+                                    onChange={(country) => {this.onChangeValue('country', country)}}
+                                    selected={this.state.user.country}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label>VAT Number: </label>
+                                {errors && errors.invite && errors.invite.vat_number ? (
+                                    <FieldError message={errors.invite.vat_number} />
+                                ) : null}
+                                <Input onChange={this.onChangeField.bind(this, 'vat_number')}
+                                       value={this.state.user.vat_number}
+                                       placeholder="VAT Number"
+                                       required/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Button type="submit">Create Client</Button>
+                            </FormGroup>
+                        </React.Fragment>
+                    )}
                 </form>
             </div>
         );
